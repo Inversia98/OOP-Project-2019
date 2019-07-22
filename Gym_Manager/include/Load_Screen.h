405e784602aa4ede04ad34af3fe563e50c0e8d57
DestructerMahad <ma02960@st.habib.edu.pk>
2019-07-22 19:59:01 +0500

    commit

---
+++ Gym_Manager/include/Load_Screen.h
@@ -0,0 +1,16 @@
+#ifndef LOAD_SCREEN_H
+#define LOAD_SCREEN_H
+
+
+class Load_Screen
+{
+    public:
+        Load_Screen();
+        virtual ~Load_Screen();
+
+    protected:
+
+    private:
+};
+
+#endif // LOAD_SCREEN_H
---
+++ Gym_Manager/src/Load_Screen.cpp
@@ -0,0 +1,11 @@
+#include "Load_Screen.h"
+
+Load_Screen::Load_Screen()
+{
+    //ctor
+}
+
+Load_Screen::~Load_Screen()
+{
+    //dtor
+}
