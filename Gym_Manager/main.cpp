6ec4b0fd4aa9b3825ea288a603d6ac05fdd4a2de
Farjad <sz00517@st.habib.edu.pk>
2019-07-24 12:48:08 +0500

    Update main.cpp

--- Gym_Manager/main.cpp
+++ Gym_Manager/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <stdlib.h>
+#include <strings.h>
+#include <time.h>
+#include <SDL2/SDL.h>
+#include "LTexture.h"
+#include "Load_Screen.h"
+#include "Buttons.h"
+#include "End_Screen.h"
+
 
 using namespace std;
 
+// Predefined Screen width and Height.
+const int SCREEN_WIDTH = 1024;
+const int SCREEN_HEIGHT = 600;
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+  Load_Screen* loadscreen = new Load_Screen(&gSpriteSheetTexturem, (float)SCREEN_WIDTH/2, (float)SCREEN_HEIGHT/2, &gSpriteSheetTexturew);
 }
