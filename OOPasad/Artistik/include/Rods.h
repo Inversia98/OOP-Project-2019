9242259ac6a7e7c59ab2af676d86de1084da5379
DestructerMahad <ma02960@st.habib.edu.pk>
2019-07-27 09:26:58 +0500

    j

--- OOPasad/Artistik/include/Rods.h
+++ OOPasad/Artistik/include/Rods.h
@@ -9,7 +9,12 @@ class Rods : public Weights
     public:
         Rods();
         virtual ~Rods();
+<<<<<<< HEAD
         void render(*SDL_Renderer gRenderer);
+=======
+        void render(SDL_Renderer* gRenderer)
+
+>>>>>>> 50fc4b29961964eb5ba6515205fbbf927296bee0
     protected:
 
     private:
--- OOPasad/Artistik/src/Rods.cpp
+++ OOPasad/Artistik/src/Rods.cpp
@@ -9,8 +9,14 @@ Rods::~Rods()
 {
     //dtor
 }
+<<<<<<< HEAD
 
 void Rods::render(SDL_Renderer* gRenderer)
 {
     spritesheetTexture->Render(posX,posY,&spriteclips);
+=======
+void Rods::render(SDL_Renderer* gRenderer)
+{
+    spriteSheetTexture->render(posX,posY,&spriteclips);
+>>>>>>> 50fc4b29961964eb5ba6515205fbbf927296bee0
 }
