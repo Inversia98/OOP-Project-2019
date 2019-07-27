dbac3ef0a3c4cc26fe0e44fc426d8e7a6be8ad10
DestructerMahad <ma02960@st.habib.edu.pk>
2019-07-27 09:28:22 +0500

    commit

---
+++ OOPasad/Artistik/Rods.h
@@ -0,0 +1,16 @@
+#ifndef RODS_H
+#define RODS_H
+#include "Weights.h"
+
+class Rods :: public Weights
+{
+    public:
+        Rods();
+        ~Rods();
+
+    protected:
+
+    private:
+};
+
+#endif // RODS_H
